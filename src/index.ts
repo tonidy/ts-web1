9a9e9059df4ff5028ac204907bb4c1a4a35851ba
toni dy <tonidy@users.noreply.github.com>
2025-01-03 15:24:46 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,6 @@ const server = http.createServer((req, res) => {
 });
 
 // Start the server on port 8080
-server.listen(8080, () => {
-  console.log("Server is running on http://:8080");
+server.listen(3000, () => {
+  console.log("Server is running on http://:3000");
 });
